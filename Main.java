9a11203954605509bc6192a70d5bcd4416dc0f3c
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-15 18:04:55 -0300

    funcao de remover tarefa

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ public class Main{
             System.out.println("1 - Adicionar Tarefa");
             System.out.println("2 - Listar Tarefas");
             System.out.println("3 - Marcar tarefa como concluida");
+            System.out.println("4 - Remover tarefa");
             System.out.println("0 - Sair!");
             opcao = leitor.nextInt();
             leitor.nextLine();
@@ -30,30 +31,15 @@ public class Main{
                     System.out.print("\n");
                     break;
                 case 2:
-                System.out.print("\n");
+                    if(verificaListaVazia(tarefas)){
+                    System.out.print("\n");
                     System.out.println("Listando todas as tarefas: ");
-                    int i = 1;
-                    for(Tarefa t : tarefas){
-                        if(t.isConcluida()){
-                            System.out.println(i+" - "+ t.getDescricao() + "[Concluida]");
-                        }else{
-                            System.out.println(i+" - "+ t.getDescricao() + "[Pendente]");
-                        }
-                        System.out.print("\n");
-                        i++;
+                    listarTarefas(tarefas);
                     }
                     break;
                 case 3:
-                int j = 1;
-                    for(Tarefa t : tarefas){
-                        if(t.isConcluida()){
-                            System.out.println(j+" - "+ t.getDescricao() + "[Concluida]");
-                        }else{
-                            System.out.println(j+" - "+ t.getDescricao() + "[Pendente]");
-                        }
-                        System.out.print("\n");
-                        j++;
-                    }
+                    if(verificaListaVazia(tarefas)){
+                    listarTarefas(tarefas);
                     System.out.print("\n");
                     System.out.println("Qual das tarefas acima voce quer marcar como conluida: ");
                     int numTarefa = leitor.nextInt();
@@ -61,12 +47,49 @@ public class Main{
                     int index = numTarefa - 1;
                     Tarefa tarefaParaMarcar = tarefas.get(index);
                     tarefaParaMarcar.setConcluida(true);
+                    }
+                    break;
+                case 4:
+                    if(verificaListaVazia(tarefas)){
+                    listarTarefas(tarefas);
+                    System.out.println("Qual das tarefas acima voce quer remover: ");
+                    int numTarefaRemover = leitor.nextInt();
+                    leitor.nextLine();
+                    int indexRemocao = numTarefaRemover - 1;
+                    tarefas.remove(indexRemocao);
+                    System.out.println("Tarefa removida com sucesso!");
+                    }
                     break;
                 case 0:
                     System.out.println("Saindo... ");
                     break;
             }
            
+        }
+    }
+
+    public static void listarTarefas(ArrayList<Tarefa> listaParaImprimir){
+        System.out.print("\n");
+        int i = 1;
+        for(Tarefa t : listaParaImprimir){
+            if(t.isConcluida()){
+                System.out.println(i+" - "+ t.getDescricao() + "[Concluida]");
+            }else{
+                System.out.println(i+" - "+ t.getDescricao() + "[Pendente]");
+            }
+            System.out.print("\n");
+            i++;
+        }
+    }
+
+    public static boolean verificaListaVazia(ArrayList<Tarefa> lista){
+        if(lista.size()<=0){
+            System.out.println("\n");
+            System.out.println("Voce nao tem tarefas!");
+            System.out.println("\n");
+            return false;
+        }else{
+            return true;
         }
     }
 }

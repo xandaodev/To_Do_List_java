6d51d829ced7c1ca4dc889dd49fda75a7a614bed
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-14 22:25:21 -0300

    teste inicial

---
+++ Main.java
@@ -0,0 +1,9 @@
+package To_Do_List_java;
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello word!");
+
+        public Scanner leitor = new Scanner(System.in); 
+    }
+}

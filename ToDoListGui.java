7a3698e896137947cfb2976ae09ea0e25f5fb8b5
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-18 16:03:12 -0300

    prmieros passos GUI

--- ToDoListGui.java
+++ ToDoListGui.java
@@ -1,5 +1,42 @@
 package To_Do_List_java;
 
-public class TodoListGui {
-    
+import javax.swing.*; 
+import java.awt.*;
+
+public class ToDoListGui extends JFrame {
+
+    public ToDoListGui(){
+        super("Minha Lista de Tarefas"); 
+        this.setSize(400, 600); 
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        this.setLocationRelativeTo(null);
+        
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+
+        JPanel painelEntrada = new JPanel();
+        painelEntrada.setLayout(new BorderLayout());
+
+        JTextField campoTarefa = new JTextField();
+        JButton botaoAdicionar = new JButton("Adicionar");
+
+        painelEntrada.add(campoTarefa, BorderLayout.CENTER);
+        painelEntrada.add(botaoAdicionar, BorderLayout.EAST);
+
+        this.add(painelEntrada, BorderLayout.NORTH);
+        
+        JList<String> listaVisual = new JList<>();
+        JScrollPane scrollLista = new JScrollPane(listaVisual); 
+        
+        this.add(scrollLista, BorderLayout.CENTER);
+
+    }
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+            new ToDoListGui();
+        });
+    }
+
+
 }
